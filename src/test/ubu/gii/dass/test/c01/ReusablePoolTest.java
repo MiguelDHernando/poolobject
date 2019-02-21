93d9dbba683c805ca2ab8e56c683b9a775f3b89e
Ebl0010 <ebl0010@alu.ubu.es>
2019-02-21 19:08:23 +0100

    Update ReusablePoolTest.java
    Test releusableRelease implementado

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.DuplicatedInstanceException;
 import ubu.gii.dass.c01.NotFreeInstanceException;
 import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
@@ -44,34 +45,35 @@ public class ReusablePoolTest {
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
-	@Test
-	public void testAcquireReusable() {
-		int i = 3;
-		try {
-			ReusablePool p = ReusablePool.getInstance();
-			Reusable r;
-			i = 3;
-			while (i > 0) {
-				r = p.acquireReusable();
-				i --;
-			}
-			fail("Deberia haber lanzado excepcion por máximo de instancias utilizadas");
-			
-		} catch (NotFreeInstanceException e) {
-			System.out.println("lanzada excepcion esperada, limite maximo de instancias");
-			//e.printStackTrace();
-			assertEquals(i, 1);
-		}
-		
-		
-	}
+	
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
 	 */
 	@Test
 	public void testReleaseReusable() {
-		fail("Not yet implemented");
+		
+		Reusable r1 = new Reusable();
+		Reusable r2 = new Reusable();
+		ReusablePool p = ReusablePool.getInstance();
+		int releasados = 0;
+		
+		try {
+			p.releaseReusable(r1);
+			releasados ++;
+			p.releaseReusable(r2);
+			releasados ++;
+			assertEquals(2, releasados);
+			p.releaseReusable(r2);
+			releasados ++;
+			fail("se ha podido releasar una instancia ya releasada");
+		} catch (DuplicatedInstanceException e) {
+			System.out.println("RELEASEREUSABLE TEST: lanzada excepcion esperada, releasada instancia repetida");
+			//e.printStackTrace();
+			assertEquals(releasados, 2);
+		}
+		
+		
 	}
 
 }
